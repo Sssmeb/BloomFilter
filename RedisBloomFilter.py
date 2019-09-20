ddb697f9f9af1b5cf876a46263d60f80fdf86b11
smeb <351108248@qq.com>
2019-09-20 16:16:02 +0800

    finish redisfilter

--- RedisBloomFilter.py
+++ RedisBloomFilter.py
@@ -7,11 +7,12 @@
 import redis
 from BloomFilter import BloomFilter
 import mmh3
+import math
 
 
 class RedisFilter(BloomFilter):
 
-    def __init__(self, host='localhost', port=6379, db=0, key='bloomfilter'):
+    def __init__(self, host='localhost', port=6379, db=0, redis_key='bloomfilter_'):
         """
 
         :param host:
@@ -20,7 +21,7 @@ class RedisFilter(BloomFilter):
         :param key:
         """
         self.server = redis.Redis(host=host, port=port, db=db)
-        self.key = key
+        self.redis_key = redis_key
 
     def start(self, data_size, error_rate=0.001):
 
@@ -33,9 +34,10 @@ class RedisFilter(BloomFilter):
         self.error_rate = error_rate
 
         bit_num, hash_num = super()._adjust_param(data_size, error_rate)
-        self._bit_num = bit_num if bit_num < (1 << 31) else (1 << 31)
+        self._bit_num = bit_num if bit_num < (1 << 32) else (1 << 32)
         self._hash_num = hash_num
-
+        # redis字符串最长为512M
+        self._block_num = 1 if bit_num < (1 << 32) else math.ceil(math.ceil(bit_num/8/1024/1024)/512)
         # 将哈希种子固定为 1 - hash_num （预留持久化过滤的可能）
         self._hash_seed = [i for i in range(1, hash_num+1)]
 
@@ -55,9 +57,11 @@ class RedisFilter(BloomFilter):
         if self._is_half_fill():
             raise IndexError("The capacity is insufficient")
 
+        keyname = self.redis_key + str(sum(map(ord, key)) % self._block_num)
+
         for time in range(self._hash_num):
             key_hashed_idx = mmh3.hash(key, self._hash_seed[time]) % self._bit_num
-            self.server.setbit(self.key, key_hashed_idx, 1)
+            self.server.setbit(keyname, key_hashed_idx, 1)
 
         self._data_count += 1
         return True
@@ -70,15 +74,35 @@ class RedisFilter(BloomFilter):
         判断该值是否存在
         有任意一位为0 则肯定不存在
         """
+        keyname = self.redis_key + str(sum(map(ord, key)) % self._block_num)
+
         for time in range(self._hash_num):
             key_hashed_idx = mmh3.hash(key, self._hash_seed[time]) % self._bit_num
-            if not int(self.server.getbit(self.key, key_hashed_idx)):    # 类型？
+            if not int(self.server.getbit(keyname, key_hashed_idx)):    # 类型？
                 return False
         return True
 
-    def copy(self):
-        """
-        防止调用父类copy方法
-        :return:
-        """
-        pass
+
+bf = RedisFilter()
+bf.start(100000000, 0.0001)
+# print(bf._bit_num, bf._hash_num)
+# print(bf._block_num)
+# print(bf._bit_num)
+# a = ['when', 'how', 'where', 'too', 'there', 'to', 'when']
+# for i in a:
+#     print(bf.add(i))
+#
+# print('xixi in bf?: ', 'xixi' in bf)
+#
+# b = ['when', 'xixi', 'haha']
+# for i in b:
+#     if bf.is_exists(i):
+#         print('%s exist' % i)
+#     else:
+#         print('%s not exist' % i)
+#
+# print('bf had load data: ', len(bf))
+#
+
+new_bf = bf.copy()
+print('new_bf had load data: ', len(new_bf))
